260ad88af2f0c7489e56123a07ab712d900a191d
Breno <brenoeller@hotmail.com>
2024-08-21 14:40:03 -0300

    New - Delete serie

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -4,7 +4,14 @@
     <a href="series/criar" class="btn btn-primary mt-2">Adicionar</a>
     <ul class="list-group mt-1">
         @foreach ($series as $serie)
-            <li class="list-group-item">{{ $serie->nome }}</li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                {{ $serie->nome }}
+                <form action="{{ route('series.destroy', $serie->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm">X</button>
+                </form>
+            </li>
         @endforeach
     </ul>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ Route::controller(SeriesController::class)->group(function() {
     Route::get('/series', 'index')->name('series.index');
     Route::get('/series/criar', 'create')->name('series.create');
     Route::post('/series/salvar', 'store')->name('series.store');
+    Route::delete('/series/destroy/{serie}', 'destroy')->name('series.destroy');
 });
